90f85b7ed3596253ba4d1495ddfa46c71dd835fc
smitchsmith <github@mitchellzsmith.com>
2020-03-29 19:56:23 -0400

    Redirects if not part of table

--- app/controllers/hands_controller.rb
+++ app/controllers/hands_controller.rb
@@ -1,6 +1,8 @@
 class HandsController < ApplicationController
+  before_action :find_hand, only: :show
+  before_action :redirect_if_not_part_of_table, only: :show
+
   def show
-    @hand = Hand.find(params[:id])
     @current_player_hand = @hand.player_hands.find_by(player_id: current_user.id)
   end
 
@@ -9,5 +11,17 @@ class HandsController < ApplicationController
     hand.next!(player: current_user, params: params)
     HandChannel.broadcast_to(hand, hand.id)
     redirect_to hand_path(hand.id)
+  end
+
+  private
+
+  def find_hand
+    @hand = Hand.find(params[:id])
+  end
+
+  def redirect_if_not_part_of_table
+    if @hand.table.players.exclude?(current_user)
+      redirect_to :root
+    end
   end
 end
